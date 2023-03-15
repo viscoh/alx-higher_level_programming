e80ee8890b586a10305013c8cc99aa37434316ae
sbonele <sbonelemiya@gmail.com>
2023-03-15 21:41:58 +0200

    updates

---
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -0,0 +1,8 @@
+-- lists all cities of Carlifornia in the database hbtn_0d_usa.
+
+SELECT id, name
+FROM cities 
+WHERE state_id = 
+	(SELECT id FROM states
+	 WHERE name = 'California')
+ORDER BY id ASC;
